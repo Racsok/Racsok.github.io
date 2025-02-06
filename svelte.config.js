af78ed0db6bc5f5c1c59fffcd6eead091967eb9e
Racsok <oscarmcg02@gmail.com>
2025-02-05 21:31:20 -0500

    push

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			out: 'build',
-			fallback: '404.html',
+			fallback: undefined,
 		  }),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
